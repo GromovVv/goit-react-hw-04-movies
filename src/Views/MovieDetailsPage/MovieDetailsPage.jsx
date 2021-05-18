f83bf5f0f7e210561668b58951e96752b0fc39a0
GromovVv <pavel.kuts@gmail.com>
2021-05-18 03:46:46 +0300

    review + cast

--- src/Views/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/Views/MovieDetailsPage/MovieDetailsPage.jsx
@@ -3,13 +3,13 @@ import './MovieDetailsPage.scss';
 import { Component } from 'react';
 import { NavLink, Route, Switch } from 'react-router-dom';
 
-// import Cast from './Views/Cast';
-// import Reviews from './Views/Reviews';
+import Cast from '../Cast';
+import Reviews from '../Reviews';
 
 import {
   fetchMovieById,
-  // fetchMovieCast,
-  // fetchMovieReviews,
+  fetchMovieCast,
+  fetchMovieReviews,
   IMAGE_URL,
 } from '../../Services/MovieApi';
 
@@ -24,16 +24,16 @@ class MovieDetailsPage extends Component {
     const { movieId } = this.props.match.params;
 
     const movie = await fetchMovieById(movieId);
-    // const cast = await fetchMovieCast(movieId);
-    // const reviews = await fetchMovieReviews(movieId);
+    const cast = await fetchMovieCast(movieId);
+    const reviews = await fetchMovieReviews(movieId);
 
-    // this.setState({ movie, cast, reviews });
-    this.setState({ movie });
-
-    // console.log(movie, cast, reviews);
-    // console.log(movie);
-
+    this.setState({ movie, cast, reviews });
   }
+
+  onPageBack = () => {
+    const { location, history } = this.props;
+    history.push(location?.state?.from || '/');
+  };
 
   render() {
     const {
@@ -44,27 +44,78 @@ class MovieDetailsPage extends Component {
       vote_average,
       overview,
       release_date,
-      // backdrop_path
     } = this.state.movie;
+
+    const { url, path } = this.props.match;
+
+    // const { from } = this.props.history.location.state;
+
 
     return (
       id !== undefined && (
         <div>
-        <button type="button" >Back</button>
-        <h2>{title}</h2>
-        <img src={IMAGE_URL + poster_path} alt={title} />
-        <p>{`${vote_average} of 10`}</p>
-        <p>{`Date of release: ${release_date}`}</p>
-        <p>{genres !== undefined ? genres.map(genre => genre.name).join(' / ') : ''}</p>
+          <button type="button" onClick={this.onPageBack}>Back</button>
+          <div className="card-wrapperr">
+            <h2>{title}</h2>
+            <img src={IMAGE_URL + poster_path} alt={title} />
+            <p>{`Rating:  ${vote_average} of 10`}</p>
+            <p>{`Date of release:   ${release_date}`}</p>
+            <p>
+              {genres !== undefined
+                ? genres.map(genre => genre.name).join(' / ')
+                : ''}
+            </p>
 
-        {/* <ul>
-          {genres.map(({ id, name }) => <li key={id}>{name}</li>)}
-        </ul> */}
-        <p>{overview}</p>
+            <p>{overview}</p>
+          </div>
+          <h3>Additional information</h3>
+          <ul>
+          <NavLink
+              to={{
+                pathname: `${url}/cast`,
+                // state: {
+                //   from,
+                // },
+              }}
+            >
+              Cast
+            </NavLink>
+          </ul>
+          <ul>
+            <NavLink
+              to={{
+                pathname: `${url}/reviews`,
+                // state: {
+                //   from,
+                // },
+              }}
+            >
+              Films Reviews
+            </NavLink>{' '}
+          </ul>
+          <Switch>
+            <Route
+              path={`${path}/cast`}
+              render={props => {
+                return (
+                  <Cast
+                    {...props}
+                    cast={this.state.cast}
+                    onClick={this.pageBackClick}
+                  />
+                );
+              }}
+            />
+            <Route
+              path={`${path}/reviews`}
+              render={props => {
+                return <Reviews {...props} reviews={this.state.reviews} />;
+              }}
+            />
+          </Switch>
         </div>
       )
-    )
-     
+    );
   }
 }
 
